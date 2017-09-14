626b252f7c4e6890c291a18593a225fdb85b350b
bewewer <bewewer@E1118W05>
2017-09-13 19:39:22 -0500

    P1 9.13 7:39

--- P1/P1/P1.c
+++ P1/P1/P1.c
@@ -13,11 +13,11 @@ int main()
 	}
 
 	char eq[100];
-	char check[100];
+	
 
-	while (fscanf_s(file, "%s", eq, sizeof(eq)) != EOF)
+	while (fgets(eq, 100, file) != NULL)
 	{
-		for (int i = 0; i < strlen_s(eq); i++)
+		for (int i = 0; i < strlen(eq); i++)
 		{
 			if (((int)eq[i] < 40 && (int)eq[i] != 32) || ((int)eq[i] > 57 && ((int)eq[i] != 61) && ((int)eq[i] != 91) && ((int)eq[i] != 93) && ((int)eq[i] != 123) && ((int)eq[i] != 125)))
 			{
@@ -25,12 +25,91 @@ int main()
 				break;
 			}
 		}
-		
+		check(eq);
 		
 	}
 	printf_s("Equation: \n");
-	printf_s("%600s", eq);
+	printf_s("%s", eq);
 	printf_s("Solve this shit here.");
-	scanf_s("");
+	scanf_s(" ");
 //	fclose(file);
+}
+
+
+
+void check(char eq[])
+{
+	char check[100];
+	int j = 0;
+	int h = 0;
+	for (int i = 0; i < strlen(eq); i++)
+	{
+		if ((int)eq[i] == 40)
+		{
+			check[j] == '(';
+			j++;
+		}
+		else if ((int)eq[i] == 41)
+		{
+			check[j] == ')';
+			j++;
+		}
+		else if ((int)eq[i] == 91)
+		{
+			check[j] == '[';
+			j++;
+		}
+		else if ((int)eq[i] == 93)
+		{
+			check[j] == ']';
+			j++;
+		}
+		else if ((int)eq[i] == 123)
+		{
+			check[j] == '{';
+			j++;
+		}
+		else if ((int)eq[i] == 125)
+		{
+			check[j] == '}';
+			j++;
+		}
+	}
+
+	char test[100];
+	test[0] = check[0];
+	int h = 0;
+	for (int i = 1; i < countof(check); i++)
+	{
+		if ((int)eq[i] == 40)
+		{
+			check[i] == '(';
+			h++;
+		}
+		else if ((int)eq[i] == 41)
+		{
+			check[i] == ')';
+			h++;
+		}
+		else if ((int)eq[i] == 91)
+		{
+			check[i] == '[';
+			h++;
+		}
+		else if ((int)eq[i] == 93)
+		{
+			check[i] == ']';
+			h++;
+		}
+		else if ((int)eq[i] == 123)
+		{
+			check[i] == '{';
+			h++;
+		}
+		else if ((int)eq[i] == 125)
+		{
+			check[i] == '}';
+			h++;
+		}
+	}
 }
