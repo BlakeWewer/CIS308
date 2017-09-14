19b723a68b00921c1fb4a46215171c8014961639
bewewer <bewewer@E1118W05>
2017-09-13 20:15:39 -0500

    P1 9.13 8:15

--- P1/P1/P1.c
+++ P1/P1/P1.c
@@ -2,6 +2,111 @@
 #include <stdlib.h>
 #include <string.h>
 
+void checkEq(char a[])
+{
+	char check[100];
+	int j = 0;
+	int h = 0;
+	for (int i = 0; i < strlen(a); i++)
+	{
+		if ((int)a[i] == 40)
+		{
+			check[j] = '(';
+			j++;
+		}
+		else if ((int)a[i] == 41)
+		{
+			check[j] = ')';
+			j++;
+		}
+		else if ((int)a[i] == 91)
+		{
+			check[j] = '[';
+			j++;
+		}
+		else if ((int)a[i] == 93)
+		{
+			check[j] = ']';
+			j++;
+		}
+		else if ((int)a[i] == 123)
+		{
+			check[j] = '{';
+			j++;
+		}
+		else if ((int)a[i] == 125)
+		{
+			check[j] = '}';
+			j++;
+		}
+	}
+
+	char test[100];
+	test[0] = check[0];
+	for (int i = 1; i < strlen(check); i++)
+	{
+		if ((int)check[i] == 40)
+		{
+			test[i] = '(';
+			h++;
+		}
+		else if ((int)check[i] == 41)
+		{
+			if (test[h] == 40)
+			{
+				test[h] = (char)0;
+				h--;
+			}
+			else {
+				test[i] = ')';
+				h++;
+			}
+		}
+		else if ((int)check[i] == 91)
+		{
+			test[i] = '[';
+			h++;
+		}
+		else if ((int)check[i] == 93)
+		{
+			if (test[h] == 91)
+			{
+				test[h] = (char)0;
+				h--;
+			}
+			else {
+				test[i] = ']';
+				h++;
+			}
+		}
+		else if ((int)check[i] == 123)
+		{
+			test[i] = '{';
+			h++;
+		}
+		else if ((int)check[i] == 125)
+		{
+			if (test[h] == 123)
+			{
+				test[h] = (char)0;
+				h--;
+			}
+			else {
+				test[i] = '}';
+				h++;
+			}
+		}
+	}
+
+	if (strlen(test) == 0)
+	{
+		printf_s("Equation is Correct.\n\n");
+	}
+	else {
+		printf_s("Equation is Incorrect");
+	}
+}
+
 int main()
 {
 	FILE *file;
@@ -17,120 +122,21 @@ int main()
 
 	while (fgets(eq, 100, file) != NULL)
 	{
-		for (int i = 0; i < strlen(eq); i++)
+		for (int i = 0; i < strlen(eq) - 1; i++)
 		{
-			if (((int)eq[i] < 40 && (int)eq[i] != 32) || ((int)eq[i] > 57 && ((int)eq[i] != 61) && ((int)eq[i] != 91) && ((int)eq[i] != 93) && ((int)eq[i] != 123) && ((int)eq[i] != 125)))
+			if (((int)eq[i] < 40 && (int)eq[i] != 32) || ((int)eq[i] > 57 && (((int)eq[i] != 61) && ((int)eq[i] != 91) && ((int)eq[i] != 93) && ((int)eq[i] != 123) && ((int)eq[i] != 125))))
 			{
 				printf_s("Invalid Characters.");
 				break;
 			}
 		}
-		check(eq);
-		
+		printf_s("Equation: \n");
+		printf_s("%s\n", eq);
+		checkEq(eq);
+		scanf_s(" ");
 	}
-	printf_s("Equation: \n");
-	printf_s("%s\n", eq);
-	scanf_s(" ");
 //	fclose(file);
 }
 
 
 
-void check(char a[])
-{
-	char check[100];
-	int j = 0;
-	int h = 0;
-	for (int i = 0; i < strlen(a); i++)
-	{
-		if ((int)a[i] == 40)
-		{
-			check[j] == '(';
-			j++;
-		}
-		else if ((int)a[i] == 41)
-		{
-			check[j] == ')';
-			j++;
-		}
-		else if ((int)a[i] == 91)
-		{
-			check[j] == '[';
-			j++;
-		}
-		else if ((int)a[i] == 93)
-		{
-			check[j] == ']';
-			j++;
-		}
-		else if ((int)a[i] == 123)
-		{
-			check[j] == '{';
-			j++;
-		}
-		else if ((int)a[i] == 125)
-		{
-			check[j] == '}';
-			j++;
-		}
-	}
-
-	char test[100];
-	test[0] = check[0];
-	for (int i = 1; i < countof(check); i++)
-	{
-		if ((int)check[i] == 40)
-		{
-			test[i] == '(';
-			h++;
-		}
-		else if ((int)check[i] == 41)
-		{
-			if (test[h] == 41)
-			{
-				test[h] == NULL;
-				h -= 2;
-			}
-			test[i] == ')';
-			h++;
-		}
-		else if ((int)check[i] == 91)
-		{
-			test[i] == '[';
-			h++;
-		}
-		else if ((int)check[i] == 93)
-		{
-			if (test[h] == 91)
-			{
-				test[h] == NULL;
-				h -= 2;
-			}
-			test[i] == ']';
-			h++;
-		}
-		else if ((int)check[i] == 123)
-		{
-			test[i] == '{';
-			h++;
-		}
-		else if ((int)check[i] == 125)
-		{
-			if (test[h] == 123)
-			{
-				test[h] == NULL;
-				h -= 2;
-			}
-			test[i] == '}';
-			h++;
-		}
-	}
-
-	if (countof(test) == 0)
-	{
-		printf_s("Equation is Correct.\n\n");
-	}
-	else {
-		printf_s("Equation is Incorrect");
-	}
-}
