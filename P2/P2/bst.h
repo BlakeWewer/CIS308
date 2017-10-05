97b2c924caa34734618654d96298448c575c2d96
bewewer <bewewer@E1118W02>
2017-10-05 10:26:22 -0500

    P2 10.5 1026 bst.h finished

--- P2/P2/bst.h
+++ P2/P2/bst.h
@@ -1,1 +1,14 @@
-#pragma once
+#ifndef BST_H
+#define BST_H
+
+typedef struct NODE {
+	int key;
+	NODE* left;
+	NODE* right;
+
+	void insertNode(NODE* newNode);
+	NODE searchTree(NODE* targetNode);
+	void printTree();
+};
+
+#endif
