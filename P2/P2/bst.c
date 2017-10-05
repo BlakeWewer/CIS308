5c72007a074fd2c4e08641a12be9f730330c2619
bewewer <bewewer@E1118W02>
2017-10-05 11:05:20 -0500

    P2 10.5 1105

--- P2/P2/bst.c
+++ P2/P2/bst.c
@@ -20,10 +20,26 @@ void InsertNode(struct NODE* newNode, struct NODE* head)
 	}
 }
 
+/*
 struct NODE SearchTree(int target, struct NODE * head)
 {
+	struct NODE* cur;
+	if (head->key == NULL)
+	{
+		return 
+	}
+	else {
+		if (newNode->key < head->key)
+		{
+			insertNode(newNode, head->left);
+		}
+		else if (newNode->key > head->key)
+		{
+			insertNode(newNode, head->right);
+		}
+	}
 	return NODE;
-}
+} */
 
 void PrintTree(struct NODE * head)
 {
